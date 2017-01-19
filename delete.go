10da92d36bea6cb3842520e5d06a445af5f7e11d
acoshift <acoshift@gmail.com>
2017-01-20 01:44:21 +0700

    Change design

--- delete.go
+++ delete.go
@@ -7,22 +7,42 @@ import (
 )
 
 // DeleteByID deletes data from datastore by IDKey
-func (client *Client) DeleteByID(ctx context.Context, kind KindGetter, id string) error {
+func (client *Client) DeleteByID(ctx context.Context, kind string, id int64) error {
+	if id == 0 {
+		return datastore.ErrInvalidKey
+	}
+	return client.Delete(ctx, datastore.IDKey(kind, id, nil))
+}
+
+// DeleteByStringID deletes data from datastore by IDKey
+func (client *Client) DeleteByStringID(ctx context.Context, kind string, id string) error {
 	tid := parseID(id)
 	if tid == 0 {
-		return ErrInvalidID
+		return datastore.ErrInvalidKey
 	}
-	return client.Delete(ctx, datastore.IDKey(kind.Kind(), parseID(id), nil))
+	return client.Delete(ctx, datastore.IDKey(kind, tid, nil))
 }
 
 // DeleteByIDs deletes data from datastore by IDKeys
-func (client *Client) DeleteByIDs(ctx context.Context, kind KindGetter, ids []string) error {
-	k := kind.Kind()
+func (client *Client) DeleteByIDs(ctx context.Context, kind string, ids []int64) error {
+	keys := make([]*datastore.Key, len(ids))
+	for i, id := range ids {
+		if id == 0 {
+			return datastore.ErrInvalidKey
+		}
+		keys[i] = datastore.IDKey(kind, id, nil)
+	}
+	return client.DeleteMulti(ctx, keys)
+}
+
+// DeleteByStringIDs deletes data from datastore by IDKeys
+func (client *Client) DeleteByStringIDs(ctx context.Context, kind string, ids []string) error {
+	k := kind
 	keys := make([]*datastore.Key, len(ids))
 	for i, id := range ids {
 		tid := parseID(id)
 		if tid == 0 {
-			return ErrInvalidID
+			return datastore.ErrInvalidKey
 		}
 		keys[i] = datastore.IDKey(k, tid, nil)
 	}
@@ -30,22 +50,21 @@ func (client *Client) DeleteByIDs(ctx context.Context, kind KindGetter, ids []st
 }
 
 // DeleteByName deletes data from datastore by NameKey
-func (client *Client) DeleteByName(ctx context.Context, kind KindGetter, name string) error {
+func (client *Client) DeleteByName(ctx context.Context, kind string, name string) error {
 	if name == "" {
-		return ErrInvalidID
+		return datastore.ErrInvalidKey
 	}
-	return client.Delete(ctx, datastore.NameKey(kind.Kind(), name, nil))
+	return client.Delete(ctx, datastore.NameKey(kind, name, nil))
 }
 
 // DeleteByNames deletes data from datastore by NameKeys
-func (client *Client) DeleteByNames(ctx context.Context, kind KindGetter, names []string) error {
-	k := kind.Kind()
+func (client *Client) DeleteByNames(ctx context.Context, kind string, names []string) error {
 	keys := make([]*datastore.Key, len(names))
 	for i, name := range names {
 		if name == "" {
-			return ErrInvalidID
+			return datastore.ErrInvalidKey
 		}
-		keys[i] = datastore.NameKey(k, name, nil)
+		keys[i] = datastore.NameKey(kind, name, nil)
 	}
 	return client.DeleteMulti(ctx, keys)
 }
--- get.go
+++ get.go
@@ -2,57 +2,9 @@ package ds
 
 import (
 	"context"
-	"reflect"
 
 	"cloud.google.com/go/datastore"
 )
-
-// SetKey sets key to model
-func SetKey(key *datastore.Key, dst interface{}) {
-	if x, ok := dst.(KeySetter); ok {
-		x.SetKey(key)
-	}
-}
-
-// SetKeys sets keys to models
-func SetKeys(keys []*datastore.Key, dst interface{}) {
-	xs := reflect.ValueOf(dst)
-	if xs.Kind() == reflect.Ptr {
-		xs = xs.Elem()
-	}
-	for i := 0; i < xs.Len(); i++ {
-		if x, ok := xs.Index(i).Interface().(KeySetter); ok {
-			x.SetKey(keys[i])
-		}
-	}
-}
-
-// SetCommitKey sets commit pending key to model
-func SetCommitKey(commit *datastore.Commit, pendingKey *datastore.PendingKey, dst interface{}) {
-	if dst == nil {
-		return
-	}
-	if x, ok := dst.(KeySetter); ok {
-		x.SetKey(commit.Key(pendingKey))
-	}
-}
-
-// SetCommitKeys sets commit pending keys to models
-func SetCommitKeys(commit *datastore.Commit, pendingKeys []*datastore.PendingKey, dst interface{}) {
-	xs := reflect.ValueOf(dst)
-	if xs.Kind() == reflect.Ptr {
-		xs = xs.Elem()
-	}
-	for i := 0; i < xs.Len(); i++ {
-		x := xs.Index(i)
-		if x.IsNil() {
-			continue
-		}
-		if x, ok := x.Interface().(KeySetter); ok {
-			x.SetKey(commit.Key(pendingKeys[i]))
-		}
-	}
-}
 
 // GetByKey retrieves model from datastore by key
 func (client *Client) GetByKey(ctx context.Context, key *datastore.Key, dst interface{}) error {
@@ -74,25 +26,52 @@ func (client *Client) GetByKeys(ctx context.Context, keys []*datastore.Key, dst 
 	return nil
 }
 
+// GetByModel retrieves model from datastore by key from model
+func (client *Client) GetByModel(ctx context.Context, dst interface{}) error {
+	key := ExtractKey(dst)
+	return client.GetByKey(ctx, key, dst)
+}
+
+// GetByModels retrieves models from datastore by keys from models
+func (client *Client) GetByModels(ctx context.Context, dst interface{}) error {
+	keys := ExtractKeys(dst)
+	return client.GetByKeys(ctx, keys, dst)
+}
+
 // GetByID retrieves model from datastore by id
-func (client *Client) GetByID(ctx context.Context, id string, dst interface{}) error {
-	return client.GetByKey(ctx, datastore.IDKey(dst.(KindGetter).Kind(), parseID(id), nil), dst)
+func (client *Client) GetByID(ctx context.Context, kind string, id int64, dst interface{}) error {
+	return client.GetByKey(ctx, datastore.IDKey(kind, id, nil), dst)
 }
 
 // GetByIDs retrieves models from datastore by ids
-func (client *Client) GetByIDs(ctx context.Context, ids []string, kind KindGetter, dst interface{}) error {
-	keys := buildIDKeys(kind.Kind(), ids)
+func (client *Client) GetByIDs(ctx context.Context, kind string, ids []int64, dst interface{}) error {
+	keys := BuildIDKeys(kind, ids)
+	return client.GetByKeys(ctx, keys, dst)
+}
+
+// GetByStringID retrieves model from datastore by string id
+func (client *Client) GetByStringID(ctx context.Context, kind string, id string, dst interface{}) error {
+	tid := parseID(id)
+	if tid == 0 {
+		return datastore.ErrInvalidKey
+	}
+	return client.GetByKey(ctx, datastore.IDKey(kind, tid, nil), dst)
+}
+
+// GetByStringIDs retrieves models from datastore by string ids
+func (client *Client) GetByStringIDs(ctx context.Context, kind string, ids []string, dst interface{}) error {
+	keys := BuildStringIDKeys(kind, ids)
 	return client.GetByKeys(ctx, keys, dst)
 }
 
 // GetByName retrieves model from datastore by name
-func (client *Client) GetByName(ctx context.Context, name string, dst interface{}) error {
-	return client.GetByKey(ctx, datastore.NameKey(dst.(KindGetter).Kind(), name, nil), dst)
+func (client *Client) GetByName(ctx context.Context, kind string, name string, dst interface{}) error {
+	return client.GetByKey(ctx, datastore.NameKey(kind, name, nil), dst)
 }
 
 // GetByNames retrieves models from datastore by names
-func (client *Client) GetByNames(ctx context.Context, names []string, kind KindGetter, dst interface{}) error {
-	keys := buildNameKeys(kind.Kind(), names)
+func (client *Client) GetByNames(ctx context.Context, kind string, names []string, dst interface{}) error {
+	keys := BuildNameKeys(kind, names)
 	return client.GetByKeys(ctx, keys, dst)
 }
 
