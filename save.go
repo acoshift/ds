90c6a5fcf89003685ef460135f4d5586f6cca023
acoshift <acoshift@gmail.com>
2017-03-13 12:21:45 +0700

    remove alloc id feat

--- save.go
+++ save.go
@@ -2,8 +2,6 @@ package ds
 
 import (
 	"context"
-
-	"cloud.google.com/go/datastore"
 )
 
 func beforeSave(kind string, src interface{}) {
@@ -23,18 +21,10 @@ func beforeSave(kind string, src interface{}) {
 // SaveModel saves model to datastore
 // kind is optional, if key already set
 // if key was not set in model, will call NewKey with given kind
-// if key is incomplete key and AllocateIncompleteID is true, will call allocate ids before put model to datastore
 func (client *Client) SaveModel(ctx context.Context, kind string, src interface{}) error {
 	beforeSave(kind, src)
 
 	x := src.(KeyGetSetter)
-	if key := x.GetKey(); key.Incomplete() && client.AllocateIncompleteID {
-		keys, err := client.AllocateIDs(ctx, []*datastore.Key{key})
-		if err != nil {
-			return err
-		}
-		x.SetKey(keys[0])
-	}
 	key, err := client.Put(ctx, x.GetKey(), x)
 	x.SetKey(key)
 	if err != nil {
@@ -47,59 +37,11 @@ func (client *Client) SaveModel(ctx context.Context, kind string, src interface{
 // see more in SaveModel
 func (client *Client) SaveModels(ctx context.Context, kind string, src interface{}) error {
 	xs := valueOf(src)
-	if client.AllocateIncompleteID {
-		keys := make([]*datastore.Key, 0, xs.Len())
-		mapIndex := make(map[int]int)
-		for i := 0; i < xs.Len(); i++ {
-			inf := xs.Index(i).Interface()
-			beforeSave(kind, inf)
-			if x, ok := inf.(KeyGetSetter); ok {
-				if k := x.GetKey(); k.Incomplete() {
-					keys = append(keys, datastore.IncompleteKey(kind, nil))
-					mapIndex[len(keys)-1] = i
-				}
-			}
-		}
-		keys, err := client.AllocateIDs(ctx, keys)
-		if err != nil {
-			return err
-		}
-		for dst, src := range mapIndex {
-			xs.Index(src).Interface().(KeyGetSetter).SetKey(keys[dst])
-		}
-	} else {
-		for i := 0; i < xs.Len(); i++ {
-			x := xs.Index(i).Interface()
-			beforeSave(kind, x)
-		}
+	for i := 0; i < xs.Len(); i++ {
+		x := xs.Index(i).Interface()
+		beforeSave(kind, x)
 	}
 	err := client.PutModels(ctx, src)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// AllocateModel calls AllocateIDModel and SaveModel
-func (client *Client) AllocateModel(ctx context.Context, kind string, src interface{}) error {
-	err := client.AllocateIDModel(ctx, kind, src)
-	if err != nil {
-		return err
-	}
-	err = client.SaveModel(ctx, kind, src)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// AllocateModels calls AllocateIDModels and SaveModels
-func (client *Client) AllocateModels(ctx context.Context, kind string, src interface{}) error {
-	err := client.AllocateIDModels(ctx, kind, src)
-	if err != nil {
-		return err
-	}
-	err = client.SaveModels(ctx, kind, src)
 	if err != nil {
 		return err
 	}
